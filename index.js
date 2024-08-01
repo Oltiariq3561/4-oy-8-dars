4d0363ad2386d4bc826537a3081ec6d22463ac8b
Oltiariq3561 <oltiariq356@gmail.com>
2024-08-01 01:37:54 -0700

    tugadi

--- index.js
+++ index.js
@@ -136,7 +136,7 @@
 //             nitral.push(i)
 //         }
 //     }
-//     return res.push( musbat + manfiy)
+//     return {musbat , manfiy , nitral }
 // }
 // let res = test(arr);
 // console.log(res);
@@ -156,7 +156,146 @@
 // console.log(res);
 
 // 12. **Massivdagi har bir elementni stringga aylantirib yangi massiv yaratadigan funksiya yozing.**
-let arr = [1,2,3,4];
-function test(arg) {
-    
-}
+// let arr = [1,2,3,4];
+// function test(arg) {
+//     let res = [];
+//     for (let i = 0; i < arg.length; i++) {
+//         res.push(arg[i].toString())
+//     }
+//     return res
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 13. **Massivda juft indeksdagi elementlarni qaytaradigan funksiya yozing.**
+// let arr = [1,2,3,4];
+// function test(arg) {
+//     let res = [];
+//     for (let i = 1; i < arg.length; i+=2) {
+//         res.push(arg[i])
+//     }
+//     return res
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 14. **Massivda toq sonlar sonini hisoblaydigan funksiya yozing.**
+// let arr = [1,2,3,4];
+// function test(arg) {
+//     let res = 0;
+//     for (let i = 0; i < arg.length; i++) {
+//         if (arg[i] % 2 == 1) {
+//             res++
+//         }
+        
+//     }
+//     return res
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 16. **Massivdagi barcha elementlarning qiymatini ikki marta takrorlaydigan funksiya yozing.**
+// let arr = [1,2,3];
+// let son = 2;
+// function test(arr,son) {
+//     let res = [];
+//     for (let i = 0; i < son; i++) {
+//        res = res.concat(arr)
+        
+//     }
+//     return res
+// }
+// let res = test(arr,son);
+// console.log(res);
+
+// 19-masala
+
+// function notStr(arr){
+//     let newArr = [];
+//     for(let i = 0; i < arr.length; i++){
+//         if(typeof arr[i] === 'string'){
+//             newArr.push(arr[i]);
+//         }
+//     }
+//     return newArr
+// }
+// let res = notStr([1,2,3,"hello","bye"]);
+// console.log(res);
+
+// 20-masala
+
+// function boshStr(arr){
+//     let counter = 0;
+//     for(let i = 0; i< arr.length; i++){
+//         if(arr[i] === " "){
+//             counter++
+//         }
+//     }
+//     return counter
+// }
+// let res = boshStr([" ","salom"," ", "hayr"]);
+// console.log( res);
+
+
+// 21-masala
+
+// function teskariMatn(arr){
+//     let words = arr.reverse();
+//     return words.join(" ")
+// }
+// let res = teskariMatn(["hayr","salom"]);
+// console.log(res); 
+
+// 22-masala
+
+// function kvIldiz(arr){
+//     // for(let i = 0; i < arr.length; i++){
+//         return arr.map(Math.sqrt);
+//     // }
+
+// }
+// let res = kvIldiz([1,4,9])
+// console.log(res);
+
+// 23-masala
+
+// function musbat(arr){
+//     let m = [];
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] > 0){
+//             m.push(arr[i]);
+//         }
+//     }
+//     return m;
+// }
+// let res = musbat([1,-2,3,-4,5,-6]);
+// console.log("musbat sonlar:",res);
+
+// 24-masala
+
+// function toqVaJuft(arr){
+//     let juft = [];
+//     let toq = [];
+//     for(let i = 0; i < arr.length; i++){
+//         if(i % 2 == 0){
+//             juft.push(arr[i]);
+//         } else{
+//             toq.push(arr[i]);
+//         }
+//     }
+//     return {toq, juft}
+// }
+// let res = toqVaJuft([1,2,3,4,5])
+// console.log(res);
+
+// 25-masala
+
+// function factorial(arr){
+//     let p = 1;
+//     for(let i = 0; i < arr.length; i++){
+//         p*=arr[i]
+//     }
+//     return p;
+// }
+// let res = factorial([1,2,3,4,5]);
+// console.log(res);
